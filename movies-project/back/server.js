2bce202a1bcdb4f5e5c2eb65fce2b7556dfb3ce6
ahmadyassinkhoja <ahmad.mohammad.khoja@gmail.com>
2018-10-31 09:49:35 +0200

    update is finished

--- movies-project/back/server.js
+++ movies-project/back/server.js
@@ -36,30 +36,6 @@ mongodb.MongoClient.connect(mongoUrl, function (err, database) {
 
 })
 
-// let ids = 3
-
-// const movies = [
-//     {
-//       name: 'Harry Potter',
-//       genre: 'Magic',
-//       length: 126,
-//       image: '../../assets/images/harry.png'
-//     },
-//     {
-//       name: 'Naruto',
-//       genre: 'Action',
-//       length: 150,
-//       image:  '../../assets/images/naruto.png'
-//     },
-//     {
-//       name: 'One Piece',
-//       genre: 'Adventure',
-//       length: 175,
-//       image:  '../../assets/images/lofi.png'
-//     },
-    
-//   ]
-
   // for parsing and delevering the json
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}) );
@@ -82,18 +58,12 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
     let movies = db.collection("movies").find({}).toArray((err, doc) => res.send(doc))
-    // res.send(movies)
 })
 app.get('/getMovie/:id', (req, res) => {
     const movie = db.collection("movies").find( { _id: ObjectID(req.params.id) } ).toArray((err, doc) => res.send(doc[0]))
-
-    // res.send(movie)
 })
 
 app.post('/addMovie', (req, res) => {
-    // req.body.id = ids++
-    // console.log(db)
-
     db.collection("movies").insertOne(req.body, function(err, result) {
         if (err) {
           console.log(err)
@@ -101,30 +71,22 @@ app.post('/addMovie', (req, res) => {
           console.log(result)
         }
     });
-    
-    // remove this one
-    // movies.push(req.body)
 })
 
 app.put('/updateMovie/:id', (req, res) => {
     console.log(req.body)
     let myQuery =  { _id: ObjectID(req.params.id) }
+
+    let movie = {
+        "name": req.body.name,
+        "genre": req.body.genre,
+        "length": req.body.length,
+    }
+
     let newValue = {$set: req.body}
-    db.collection("movies").updateOne( myQuery, newValue, (err , collection) => {
-		if(err) console.log(err)
-            console.log("Record updated successfully");
-            // console.log(collection);
-        }
-    )
-    // { upsert: true }
+    db.collection("movies").findOneAndUpdate( myQuery, {$set: movie} )
 })
 
 app.delete('/deleteMovie/:id', (req, res) => {
     const movie = db.collection("movies").remove( { _id: ObjectID(req.params.id) } )
 })
-
- 
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running at PORT ${PORT}`)
-// })
